5bf16921e343d4707a7ba453abbf99e65158406c
imbafrank <imbafrank@126.com>
2019-03-27 20:18:11 +0800

    Method Equals and GetHashCode overrided

---
+++ HW5/HW5/Order.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+
+    /// <summary>
+    /// Order class : all orderDetails
+    /// to record each goods and its quantity in this ordering
+    /// </summary>
+    class Order {
+
+        private List<OrderDetail> details=new List<OrderDetail>();
+
+        /// <summary>
+        /// Order constructor
+        /// </summary>
+        /// <param name="orderId">order id</param>
+        /// <param name="customer">who orders goods</param>
+        public Order(uint orderId, Customer customer) {
+            Id = orderId;
+            Customer = customer;
+        }
+
+        /// <summary>
+        /// order id
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// the man who orders goods
+        /// </summary>
+        public Customer Customer { get; set; }
+
+
+        public List<OrderDetail> Details {
+            get =>this.details; }
+
+        /// <summary>
+        /// add new orderDetail to order
+        /// </summary>
+        /// <param name="orderDetail">the new orderDetail which will be added</param>
+        public void AddDetails(OrderDetail orderDetail) {
+            if (this.Details.Contains(orderDetail))  {
+                throw new Exception($"orderDetails-{orderDetail.Id} is already existed!");
+            }
+            details.Add(orderDetail);
+        }
+
+        /// <summary>
+        /// remove orderDetail by orderDetailId from order
+        /// </summary>
+        /// <param name="orderDetailId">id of the orderDetail which will be removed</param>
+        public void RemoveDetails(uint orderDetailId) {
+            details.RemoveAll(d =>d.Id==orderDetailId);
+        }
+
+        /// <summary>
+        /// override ToString
+        /// </summary>
+        /// <returns>string:message of the Order object</returns>
+        public override string ToString() {
+            String result = $"orderId:{Id}, customer:({Customer})";
+            details.ForEach(detail => result += "\n\t" + detail);
+            return result;
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj) {
+            if (obj != null && GetType() == obj.GetType())
+            {
+                Order oobj = (Order)obj;
+                if (oobj.Id == this.Id)
+                {
+                    if (oobj.Customer == this.Customer)
+                        return true;
+                    else
+                        throw new Exception("Error, same order Id with different customer!");
+                }
+            }
+
+            return false;       
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            int hashcode = 0;
+            foreach (System.Reflection.PropertyInfo info in GetType().GetProperties())
+                hashcode += info.GetHashCode();
+
+            return hashcode;
+        }
+            
+
+    }
+}
